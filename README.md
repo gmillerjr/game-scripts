6fa5bd9b7bfa07b3af259007ef53d047226c81f3
gmillerjr <39544832+gmillerjr@users.noreply.github.com>
2024-07-27 22:05:50 -0400

    Initial Push
    Push of soulmask script.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # game-scripts
 Scripts relating to dedicated server installation and configuration.
+
+# Soulmask Delete and Backup
+Soulmask saves files every 10 minutes, however does not have any action for leansing the old files.  This can cause your folder to grow very quickly.  My Server sees about 6-10 gb per day.  
+
+This script will do two things.
+1. Remove any files that have not been edited within your limit.  I keep 2 days.  Edited is an important distinction as the main save for soulmask is created when you first start, then constantly edited.  It's never regenerated.
+2. Backup files in your path as well.  This will create a zip file for the world save of all the detail in the saves folder and then place it where you specify in the path.
+
+I set this to run on a timer using scheduled tasks.  
+
+# Creating the Scheudled tasks
+Coming Soon
---
+++ soulmask/soulmaskbackups.ps1
@@ -0,0 +1,27 @@
+﻿#set the limit for how many days you want saved.  This will tell the server to delete anyting older than this number.  So -2 will mean it will delete any files that have not been edited in 2+ days.
+$limit = (Get-Date).AddDays(-2)
+#add the path to your server saves
+$path1 = "D:\AMPDatastore\Instances\A1-SoulMask01\soulmask\server\WS\Saved\Worlds\Dedicated\Level01_Main"
+
+
+#Path 1
+#Delete files that have not been eded within the $limit.
+Get-ChildItem -Path $path1 -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force
+
+#Delete any empty directories left behind after deleting the old files.  Probably not needed in this instance.
+Get-ChildItem -Path $path1 -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
+
+#backup the save files
+#what files in the folder do you want backed up.
+$sourceDirA = 'D:\AMPDatastore\Instances\A1-SoulMask01\soulmask\server\WS\Saved\Worlds\Dedicated\';
+#where do you want them to go
+$targetDirA = 'C:\backups\soulmask\'
+$targetFilenameBase = 'data-';
+
+## no changes here
+$date = get-date;
+$dateStr = $date.ToString("yyyy-MM-dd_HH-mm");
+$targetFileA = $targetDirA+$targetFilenameBase+$dateStr+'.zip';
+
+Add-Type -A 'System.IO.Compression.FileSystem';
+[IO.Compression.ZipFile]::CreateFromDirectory($sourceDirA, $targetFileA);
