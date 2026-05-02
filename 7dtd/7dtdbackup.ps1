db07be18745bb26a7ef42595ef75723cfb6e47f3
gmillerjr <39544832+gmillerjr@users.noreply.github.com>
2026-05-02 19:36:45 -0400

    added 7dtd and Valheim

---
+++ 7dtd/7dtdbackup.ps1
@@ -0,0 +1,17 @@
+﻿## Variables for backup and purge
+$sourceDir = "C:\Users\root\AppData\Roaming\7DaysToDie\";
+$targetDir = "C:\servers\backups\7dtd\";
+$targetFilenameBase = '7dtd-';
+$hours = '-24';
+
+## Running the backup
+$date = get-date;
+$dateStr = $date.ToString("yyyy-MM-dd_HH-mm");
+$targetFile = $targetDir+$targetFilenameBase+$dateStr+'.zip';
+
+Add-Type -A 'System.IO.Compression.FileSystem';
+[IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $targetFile);
+
+## Purging old data
+# Delete files older than the $limit.
+Get-ChildItem -Path $targetDir -Recurse -Force | Where {$_.Lastwritetime -lt (date).addhours($hours)} | Remove-Item -Force
---
+++ Valheim/valheimbackup.ps1
@@ -0,0 +1,17 @@
+﻿## Variables for backup and purge
+$sourceDir = "C:\Users\root\AppData\LocalLow\IronGate\Valheim\";
+$targetDir = "C:\servers\backups\Valheim\";
+$targetFilenameBase = 'valheim-';
+$hours = '-24';
+
+## Running the backup
+$date = get-date;
+$dateStr = $date.ToString("yyyy-MM-dd_HH-mm");
+$targetFile = $targetDir+$targetFilenameBase+$dateStr+'.zip';
+
+Add-Type -A 'System.IO.Compression.FileSystem';
+[IO.Compression.ZipFile]::CreateFromDirectory($sourceDir, $targetFile);
+
+## Purging old data
+# Delete files older than the $limit.
+Get-ChildItem -Path $targetDir -Recurse -Force | Where {$_.Lastwritetime -lt (date).addhours($hours)} | Remove-Item -Force
